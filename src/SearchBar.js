a5c95aa21e7128b38d9c0bf847c7c775ff3ddb8c
anjali-patelnz <73802454+anjali-patelnz@users.noreply.github.com>
2021-02-10 14:11:31 +1300

    Updated Search bar spacing

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,24 +4,31 @@ import "./SearchBar.css";
 
 export default function SearchBar() {
   return (
-    <div className="container">
-      <div className="SearchBar">
-        <form className="row g-2">
-          <div className="col">
-            <div className="input-group">
-              <input type="text" placeholder="Enter a city..." />
-              <button type="submit">
-                <i className="fas fa-search"></i>
-              </button>
-            </div>
+    <div className="SearchBar">
+      <form>
+        <div className="row justify-content-center">
+          <div className="col-8">
+            <input
+              type="search"
+              id="city-search"
+              placeholder="Enter a city..."
+              autofocus="off"
+              autocomplete="off"
+            />
           </div>
-          <div className="col">
-            <button type="click">
+          <div className="col-3">
+            <button type="submit" id="search-button">
+              <i className="fas fa-search"></i>
+            </button>
+          </div>
+
+          <div class="col-1">
+            <button type="click" id="findMe-button">
               <i className="fas fa-map-marker-alt"></i>
             </button>
           </div>
-        </form>
-      </div>
+        </div>
+      </form>
     </div>
   );
 }
