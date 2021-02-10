62f4afe19ace1c978aebbdd782a2aeb944db62e9
anjali-patelnz <73802454+anjali-patelnz@users.noreply.github.com>
2021-02-11 09:54:32 +1300

    Edited the WeatherApp html and css

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -3,16 +3,18 @@ import MainWeather from "./MainWeather";
 import SearchBar from "./SearchBar";
 import Hourly from "./Hourly";
 
+import "./WeatherApp.css";
+
 export default function WeatherApp() {
   return (
     <div className="WeatherApp">
       <SearchBar />
-      <h1 id="city">Auckland</h1>
+      <h1 className="city">Auckland</h1>
       <p className="date">Sat, 7 November, 2:00</p>
       <MainWeather />
       <p className="factors">
-        Humidity <span id="humidity">90</span>% <br />
-        Wind <span id="wind">5 </span>km/h
+        Humidity: <span className="humidity">90</span>% <br />
+        Wind: <span className="wind">5 </span>km/h
       </p>
       <Hourly />
     </div>
