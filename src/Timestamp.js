8dd30cba4d9d6e2986bb2c8310c17bf9158eeae8
anjali-patelnz <73802454+anjali-patelnz@users.noreply.github.com>
2021-02-16 09:17:54 +1300

    Created all functions within the Timestamp component

--- src/Timestamp.js
+++ src/Timestamp.js
@@ -1,5 +1,40 @@
 import React from "react";
 
 export default function Timestamp(props) {
-  return <div className="Timestamp">Whats the time mr wolf</div>;
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[props.date.getDay()];
+
+  let dateMonth = props.date.getDate();
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[props.date.getMonth()];
+
+  let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return (
+    <div className="Timestamp">
+      {day}, {dateMonth} {month}, {hours}:{minutes}
+    </div>
+  );
 }
