baaf366a607979178ccfe268bb0bdd6121342997
anjali-patelnz <73802454+anjali-patelnz@users.noreply.github.com>
2021-02-16 09:51:22 +1300

    Updated the Hourly component to return the hourly timestamp and temperature functions

--- src/Hourly.js
+++ src/Hourly.js
@@ -3,12 +3,23 @@ import React from "react";
 import "./Hourly.css";
 
 export default function Hourly(props) {
+  function hourlyTimestamp() {
+    let hourlydate = new Date(props.data.dt * 1000);
+    let hours = hourlydate.getHours();
+    return `${hours}:00`;
+  }
+
+  function hourlyTemperature() {
+    let hourlyTemperature = Math.round(props.data.main.temp);
+    return `${hourlyTemperature}°`;
+  }
+
   return (
     <div className="card Hourly">
       <div className="card-body">
         <img className="card-img-top" src="" />
-        <p className="card-text">{Math.round(props.data.main.temp)}°</p>
-        <p className="card-text">15:00</p>
+        <p className="card-text">{hourlyTemperature()}</p>
+        <p className="card-text">{hourlyTimestamp()}</p>
       </div>
     </div>
   );
